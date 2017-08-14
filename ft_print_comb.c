26cc533466687d8ce7094b66303e5eedf174bebe
Chaneze AROUA <caroua@e1r3p1.42.fr>
2017-08-14 19:10:51 +0200

    Day02

---
+++ ft_print_comb.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caroua <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/03 20:55:46 by caroua            #+#    #+#             */
+/*   Updated: 2017/08/04 11:16:45 by caroua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putchar3(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+}
+
+void	ft_print_comb(void)
+{
+	char i;
+	char j;
+	char k;
+
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while (j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				ft_putchar3(i, j, k);
+				if (i != '7')
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
